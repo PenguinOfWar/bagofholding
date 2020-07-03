dc30717817a3febb71eeae05f710e6879596369d
PenguinOfWar <darryljwalker@gmail.com>
2020-07-03 13:39:32 +0000

    Deploying to gh-pages from  @ 29652baa82bf67dd4072258d9de527f3f67a79e1 🚀

---
+++ precache-manifest.fe859e1a344c74ef22908916c3e81125.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9633ab3b7a9fdae05f609f7b37fd4d2e",
+    "url": "/bagofholding/index.html"
+  },
+  {
+    "revision": "b450fc10e03a528e437e",
+    "url": "/bagofholding/static/css/main.f9ef0eef.chunk.css"
+  },
+  {
+    "revision": "c1299e2ecbde30be760e",
+    "url": "/bagofholding/static/js/2.4fde2e27.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/bagofholding/static/js/2.4fde2e27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b450fc10e03a528e437e",
+    "url": "/bagofholding/static/js/main.f4117689.chunk.js"
+  },
+  {
+    "revision": "93d34554380b176159fd",
+    "url": "/bagofholding/static/js/runtime-main.247eac61.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f23c94bcffad8fafddada198d8f9d789.js"
+  "/bagofholding/precache-manifest.fe859e1a344c74ef22908916c3e81125.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bagofholding/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
