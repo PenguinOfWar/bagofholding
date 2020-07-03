2f0f8b664698ba1438826ac698d2c1a994ddb30f
PenguinOfWar <darryljwalker@gmail.com>
2020-07-03 10:45:52 +0000

    Deploying to gh-pages from  @ 100bf7a26ad0f138f4c2ae6dcb6b3a59967a94f3 🚀

---
+++ precache-manifest.f23c94bcffad8fafddada198d8f9d789.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8b1d9c3155ff5c880178e70677f601f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "504653a1a0745ac82456",
+    "url": "/static/css/main.f9ef0eef.chunk.css"
+  },
+  {
+    "revision": "c1299e2ecbde30be760e",
+    "url": "/static/js/2.4fde2e27.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.4fde2e27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "504653a1a0745ac82456",
+    "url": "/static/js/main.edf69c3f.chunk.js"
+  },
+  {
+    "revision": "2445575ea1b2af78ac9f",
+    "url": "/static/js/runtime-main.1f72dd52.js"
+  }
+]);
