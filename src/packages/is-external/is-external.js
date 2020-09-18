56c3752bbfb79c33465e264f56c7bb0172b87dcf
Damian Ryan <damian.ryan@sony.com>
2020-09-18 16:29:21 +0100

    added www. to external detection.

--- src/packages/is-external/is-external.js
+++ src/packages/is-external/is-external.js
@@ -4,6 +4,7 @@ export default (to, customConditional) => {
   if (
     /^(f|ht)tps?:\/\//i.test(to.toLowerCase()) ||
     /^mailto:([^?]*)/.test(to.toLowerCase()) ||
+    /^(w|w)w.(.*)/.test(to.toLowerCase()) ||
     (customConditional &&
       typeof (customConditional === 'function') &&
       customConditional(to))
