f48936de4da5ecab4a47126c1a1f03de4468eb0f
Darryl Walker <darryljwalker@gmail.com>
2020-12-09 11:28:38 +0000

    Closes #2 and upgrades default node version

--- src/packages/remove-empty-props/remove-empty-props.js
+++ src/packages/remove-empty-props/remove-empty-props.js
@@ -1,19 +1,30 @@
 /**
  * Credit: rotareti@stackoverflow - https://stackoverflow.com/a/38340730
  * Modifications to resolve an object/array comparison mismatch by @penguinofwar
+ * Modifications to resolve fromEntries issues in node 10: fromentries. MIT License. Feross Aboukhadijeh <https://feross.org/opensource>
  */
+
+export const fromEntries = iterable => {
+  return [...iterable].reduce((obj, [key, val]) => {
+    obj[key] = val;
+    return obj;
+  }, {});
+};
 
 export const isObject = obj => {
   return obj && typeof obj === 'object';
 };
 
 export const isArray = object => {
-  console.log(object, object instanceof Array, typeof object, isObject(object));
   return object instanceof Array;
 };
 
-const removeEmptyProps = (object = {}) =>
-  Object.fromEntries(
+const removeEmptyProps = (object = {}) => {
+  if (!Object.fromEntries || typeof Object.fromEntries !== 'function') {
+    Object.fromEntries = fromEntries;
+  }
+
+  return Object.fromEntries(
     Object.entries(object)
       .filter(([_k, v]) => v !== null)
       .map(([k, v]) => {
@@ -26,5 +37,6 @@ const removeEmptyProps = (object = {}) =>
         return [k, v];
       })
   );
+};
 
 export default removeEmptyProps;
