f08e8cebaababa3c2bc2faf81dbc27d3487ed6a9
Darryl Walker <darryljwalker@me.com>
2021-03-19 20:49:28 +0000

    Stubbing async-timeout

---
+++ src/packages/async-timeout/async-timeout.test.js
@@ -0,0 +1,6 @@
+// import timeout from './async-timeout';
+
+it.skip('returns true when the environment is a browser and window is defined', () => {
+  // expect(isClient()).toBeTruthy();
+  // expect(isClient()).toMatchSnapshot();
+});
