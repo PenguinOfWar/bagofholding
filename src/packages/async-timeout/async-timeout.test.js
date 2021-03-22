0ea10d60e80066156417ba3449fd1926d56cef9f
Darryl Walker <darryljwalker@me.com>
2021-03-22 09:24:23 +0000

    Update async-timeout.test.js
    Testing commit signing

--- src/packages/async-timeout/async-timeout.test.js
+++ src/packages/async-timeout/async-timeout.test.js
@@ -1,5 +1,15 @@
 import timeout from './async-timeout';
 
+it('waits 250 ms', async () => {
+  const start = performance.now();
+
+  await timeout(250);
+
+  const end = Math.ceil(performance.now() - start);
+
+  expect(end).toBeGreaterThanOrEqual(250);
+});
+
 it('waits 500 ms', async () => {
   const start = performance.now();
 
