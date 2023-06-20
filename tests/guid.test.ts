69125ebadf759a9b0f2985d7fa136dce78415334
Darryl Walker <darryl.walker@jacobs.com>
2023-06-20 15:37:06 +0100

    chore: Moving guid test to tests directory

--- tests/guid.test.ts
+++ tests/guid.test.ts
@@ -1,8 +1,15 @@
 import guid from '@/packages/guid/guid';
 
-it('returns a two different guid strings', () => {
+it('returns two different guid strings', () => {
   const guid1 = guid();
   const guid2 = guid();
 
   expect(guid1).not.toBe(guid2);
+});
+
+it('created a guid in the expected format', () => {
+  const regex =
+    /^[a-zA-Z0-9]{1,16}-[a-zA-Z0-9]{1,16}_[a-zA-Z0-9]{1,16}-[a-zA-Z0-9]{1,16}_[a-zA-Z0-9]{1,16}$/gm;
+
+  expect(guid()).toMatch(regex);
 });
