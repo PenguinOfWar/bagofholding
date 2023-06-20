6d83642d7d7b6918decdae5fa2e02135aa0b5cdc
Darryl Walker <94604001+DarrylWalkerJacobs@users.noreply.github.com>
2023-06-20 15:40:15 +0100

    chore: Moving guid test to tests directory (#48)
    * chore: Moving guid test to tests directory
    
    * chore: deleting test file from guid directory

--- src/packages/guid/guid.test.js
+++
@@ -1,7 +0,0 @@
-import guid from './guid';
-
-it('created a guid in the expected format', () => {
-  const regex = /^[a-zA-Z0-9]{1,16}-[a-zA-Z0-9]{1,16}_[a-zA-Z0-9]{1,16}-[a-zA-Z0-9]{1,16}_[a-zA-Z0-9]{1,16}$/gm;
-
-  expect(guid()).toMatch(regex);
-});
